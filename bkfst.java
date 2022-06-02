e43e47b0dc26c8387b8b70c298f43f8793b3fd88
Anandkalp <anandk.neo@gmail.com>
2022-06-02 11:39:39 +0530

    lol

---
+++ bkfst.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class breakfast
+{
+public static void main(String args[])
+
+{
+Scanner sc = new Scanner(System.in);
+System.out.print("Dosa =");
+int x1 = sc.nextInt();
+
+System.out.print("OnionDosa =");
+int x2 = sc.nextInt();
+
+System.out.println("");
+
+float sum = x1+x2;
+System.out.println("Sum : "+sum);
+}
+}
